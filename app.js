5d2dce31bf2f20e46575afac556fca6fc0ce4c77
Matt Wawiorka <mattwawiorka@fb.com>
2021-10-02 15:04:25 -0700

    switching to use prototypical inheritance

--- app.js
+++ app.js
@@ -1,46 +1,71 @@
-const makeDraggable = (element) => {
-    let draggable;
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (element.className.includes("toolbar")) {
-        draggable = element.parentNode;
-    } else {
-        draggable = element;
-    }
-
-    const stopMouseDrag = () => {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-
-    const mouseDrag = (e) => {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        if (!draggable.maxed) {
-            draggable.style.left = (draggable.offsetLeft - pos1) + "px";
-            draggable.style.top = (draggable.offsetTop - pos2) + "px";
-        } 
-    }
-
-    const dragMouseDown = (e) => {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmousemove = mouseDrag;
-        document.onmouseup = stopMouseDrag;
-    }
-
-    element.onmousedown = dragMouseDown;
+function Draggable(left = 24, top = 24) {
+    this.element = document.createElement('div');
+    this.element.style.left = left + 'px';
+    this.element.style.top = top + 'px';
+    this.pos1 = 0;
+    this.pos2 = 0;
+    this.pos3 = 0;
+    this.pos4 = 0;
+    this.element.onmousedown = this.mouseDown.bind(this); // Need to bind to Draggable, otherwise this = the element
 }
 
-const draggables = document.getElementsByClassName("draggable");
-for (let element of draggables) {
-    makeDraggable(element);
+Draggable.prototype.stopDrag = function() {
+    document.onmouseup = null;
+    document.onmousemove = null;
 }
+
+Draggable.prototype.mouseDrag = function(e) {
+    e = e || window.event;
+    e.preventDefault();
+    this.pos1 = this.pos3 - e.clientX;
+    this.pos2 = this.pos4 - e.clientY;
+    this.pos3 = e.clientX;
+    this.pos4 = e.clientY;
+    this.element.style.left = (this.element.offsetLeft - this.pos1) + "px"
+    this.element.style.top = (this.element.offsetTop - this.pos2) + "px"
+}
+
+Draggable.prototype.mouseDown = function(e) {
+    e = e || window.event;
+    e.preventDefault();
+    this.pos3 = e.clientX;
+    this.pos4 = e.clientY;
+    document.onmousemove = (e) => {
+        this.mouseDrag(e)
+    };
+    document.onmouseup = this.stopDrag;
+}
+
+function DesktopIcon(image, text, left, top) {
+    Draggable.call(this, left, top);
+
+    const imageElem = document.createElement('img');
+    const p = document.createElement('p');
+
+    this.element.setAttribute('class', 'desktop-icon');
+
+    imageElem.setAttribute('src', "images/" + image + ".png");
+    this.element.appendChild(imageElem)
+
+    p.innerHTML = text;
+    this.element.appendChild(p)
+}
+
+function addToDOM(parentId) {
+    document.getElementById(parentId).appendChild(this.element)
+}
+
+DesktopIcon.prototype = Object.create(Draggable.prototype);
+DesktopIcon.prototype.constructor = DesktopIcon;
+
+DesktopIcon.prototype.addToDom = addToDOM;
+
+const test = new DesktopIcon("folder", "folder");
+test.addToDom('home')
+const test2 = new DesktopIcon("folder", "test2", 240);
+test2.addToDom('home')
+
+
 
 const clickables = document.getElementsByClassName("clickable");
 for (let element of clickables) {
@@ -124,4 +149,3 @@ document.getElementById("shutdown-no").onclick = () => {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("shutdown-warning").style.display = "none";
 }
-
